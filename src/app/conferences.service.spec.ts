12876331b94a78395a0b1fbe2c6afda30a97f2eb
Sebastian Gomez <seagomezar@gmail.com>
2018-08-20 11:53:17 -0500

    Testing the http request completely. 14. Good for now, but we still need to test the HTTP request itself. It is obvious we are not going to call the real service instead of that we are going to intercept the request and provide a fake answer, however, a lot of injections happened here, the HttpClient, the HttpTestingModule, and HttpTestingController, also we can see how we are going to flush the request just to complete it. The key core concepts here are httpTestingController.expectOne, and the flush to intercept the request and to send and response instead of the request.

--- src/app/conferences.service.spec.ts
+++ src/app/conferences.service.spec.ts
@@ -1,12 +1,17 @@
 
 import { ConferencesService } from './conferences.service';
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+// Other imports
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 
 describe('Provider: Conferences Service', () => {
+
   let conferencesService: ConferencesService;
   let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -17,10 +22,31 @@ describe('Provider: Conferences Service', () => {
       ]
     });
     httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
     conferencesService = TestBed.get(ConferencesService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(conferencesService).toBeTruthy(); // We hope the service has been inyected and defined
+  });
+
+  it('should be able to get All The conferences', () => {
+
+    const expectedConferences: any = { September: [{ name: 'NG-COL' }] };
+    conferencesService
+      .getAllConferences()
+      .subscribe((response) => {
+        expect(response.September[0].name).toBe('NG-COL');
+      });
+    // HeroService should have made one request to GET heroes from expected URL
+    const req = httpTestingController.expectOne(conferencesService.conferencesURL);
+    expect(req.request.method).toEqual('GET');
+    // Respond with the mock heroes
+    req.flush(expectedConferences);
   });
 });
