7b700ac69c9bb1eeca802c47d7d1452b4bfd2b2a
Sebastian Gomez <seagomezar@gmail.com>
2018-09-03 10:06:48 -0500

    Understanding the automatic change detection Now we understand how to handle the change detection it is complete healthy try to avoid the fixture.detectChanges when possible, because we really don't want to be aware all the time if we need to run the detectChanges statement. Angular provides us the way to handle this process very easy through ComponentFixtureAutoDetect, let's take a look the considerations and limitations about.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +8,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     }).compileComponents();
   }));
 
@@ -23,13 +26,12 @@ describe('LoginComponent', () => {
 
   it('should read proper message in proper time', (done) => {
     const p = fixture.nativeElement.querySelector('p');
-    // Phase 1 empty string
-    expect(p.textContent).toBe(' ');
-    fixture.detectChanges();
+    /* Phase 1 empty string needs to be remove because since the detect changes is triggered every time
+     * we don't have control in the very first time.
+     * expect(p.textContent).toBe(' '); */
     // Phase 2 Loading something
     expect(p.textContent).toContain('Ey! We are loading something');
     setTimeout(() => {
-      fixture.detectChanges();
       // Phase 3 content loaded
       expect(p.textContent).toContain('Something has been loaded!');
       done();
